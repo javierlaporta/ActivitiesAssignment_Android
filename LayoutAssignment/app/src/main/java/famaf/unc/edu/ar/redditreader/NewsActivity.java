f193fcaf289f0def3c70a7cb16a230584bc51258
javier <@>
2016-09-26 21:58:26 -0300

    Reddit icon mdpi,hdpi,xxhdpi,etc

--- LayoutAssignment/app/src/main/java/famaf/unc/edu/ar/redditreader/NewsActivity.java
+++ LayoutAssignment/app/src/main/java/famaf/unc/edu/ar/redditreader/NewsActivity.java
@@ -7,6 +7,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.Menu;
+//import android.view.MenuInflater;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.widget.TextView;
 
 public class NewsActivity extends AppCompatActivity {
 
