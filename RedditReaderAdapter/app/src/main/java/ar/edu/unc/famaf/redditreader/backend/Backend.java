29d58ba815d050c8b486bc166517630fc74bda7a
javier <@>
2016-10-07 16:43:40 -0300

    Post Model ok

--- RedditReaderAdapter/app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
+++ RedditReaderAdapter/app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
@@ -15,7 +15,17 @@ public class Backend {
     }
 
     public List<PostModel> getTopPosts() {
-        // TODO: implement me
+        PostModel post1 = new PostModel();
+        post1.setTitle("Google Pixel XL 128GB Smartphone Goes Out of Stock");
+        post1.setAuthor("/r/Android ");
+        post1.setDate("Hace 14 hs");
+        post1.setComment(294);
+        post1.setImageResourceId(1);
+
+        PostModel post2 = new PostModel();
+
+
+
 
         return null;
     }
