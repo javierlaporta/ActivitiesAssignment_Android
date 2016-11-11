61a206beb10796addcad2f796dc72b9ff8b4c6ed
javier <@>
2016-11-11 20:48:19 -0300

    Toda la reforma de backend OK. Proximo paso hacer el scrolling

--- RedditReader-endlessScrolling_assignment/app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
+++ RedditReader-endlessScrolling_assignment/app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
@@ -1,7 +1,15 @@
 package ar.edu.unc.famaf.redditreader.backend;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unc.famaf.redditreader.model.PostModel;
 import ar.edu.unc.famaf.redditreader.ui.PostsIteratorListener;
 
 /**
@@ -18,9 +26,63 @@ public class Backend {
     }
 
     public void getNextPosts(final PostsIteratorListener listener, Context context) {
+        // HAY QUE REFORMAR ACAAAAAAAAAAAAAAAA
         //aca voy a tener q hacer que me lea de a 5 posts accediendo a la base de datos
         //voy a tener q llamar a listener.nextPost
 
+        final RedditDBHelper db = new RedditDBHelper(context);
+
+        if (isConnected(context)){
+            new GetTopPostsTask() {
+                @Override
+                protected void onPostExecute(List<PostModel> postModels) {
+                    super.onPostExecute(postModels);
+                    Object[] objectArray = new Object[2];
+                    objectArray[0]= postModels;
+                    objectArray[1]= db;
+                    new WriteDatabaseTask(){
+                    }.execute(objectArray);
+                }
+            }.execute();
+        }
+        List<PostModel> postModelList = new ArrayList<>();
+        SQLiteDatabase readableDatabase = db.getReadableDatabase();
+        Cursor cursor = readableDatabase.rawQuery("SELECT * FROM " + RedditDBHelper.POST_TABLE, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                PostModel postModel = new PostModel();
+                postModel.setTitle(cursor.getString(1));
+                postModel.setAuthor(cursor.getString(2));
+                postModel.setDate(cursor.getString(3));
+                postModel.setComment(cursor.getString(4));
+                postModel.setimageResourceUrl(cursor.getString(5));
+                postModelList.add(postModel);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        listener.nextPosts(postModelList);
+    }
+
+    public boolean isConnected(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null) { // tengo conexion a internet
+            return netInfo.getType() == ConnectivityManager.TYPE_WIFI ||
+                    netInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+        } else {
+            return false;
+        }
+    }
+    public boolean isEmpty (Context context){//para saber si la base de datos esta vacia
+        RedditDBHelper db = new RedditDBHelper(context);
+        SQLiteDatabase readableDatabase = db.getReadableDatabase();
+        Cursor cursor = readableDatabase.rawQuery(
+                " SELECT * FROM " + RedditDBHelper.POST_TABLE ,null);
+        return (! cursor.moveToFirst());
     }
 }
 
--- RedditReader-endlessScrolling_assignment/app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsActivityFragment.java
+++ RedditReader-endlessScrolling_assignment/app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsActivityFragment.java
@@ -31,85 +31,40 @@ import ar.edu.unc.famaf.redditreader.model.PostModel;
  */
 public class NewsActivityFragment extends Fragment implements PostsIteratorListener {
 
+    private View v;
+
     public NewsActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_news, container, false);
-        final ListView postLv = (ListView) v.findViewById(R.id.postLV);
-        final RedditDBHelper db = new RedditDBHelper(getContext());
+        v = inflater.inflate(R.layout.fragment_news, container, false);
 
-        if(!isConnected(getActivity())){
+        if(!Backend.getInstance().isConnected(getContext())){
             showToast(getContext());
-            List<PostModel> postModelList = new ArrayList<>();
-            SQLiteDatabase readableDatabase = db.getReadableDatabase();
-            Cursor cursor = readableDatabase.rawQuery("SELECT * FROM " + RedditDBHelper.POST_TABLE, null);
-
-            if (cursor.moveToFirst()) {
-                do {
-                    PostModel postModel = new PostModel();
-                    postModel.setTitle(cursor.getString(1));
-                    postModel.setAuthor(cursor.getString(2));
-                    postModel.setDate(cursor.getString(3));
-                    postModel.setComment(cursor.getString(4));
-                    postModel.setimageResourceUrl(cursor.getString(5));
-                    postModelList.add(postModel);
-                } while (cursor.moveToNext());
-            }
-            cursor.close();
-            db.close();
-            PostAdapter adapter = new PostAdapter(getContext(), R.layout.porst_row, postModelList);
-            postLv.setAdapter(adapter);
         }
-        else {
-//            new GetTopPostsTask() {
-//                @Override
-//                protected void onPostExecute(List<PostModel> postModels) {
-//                    super.onPostExecute(postModels);
-//                    PostAdapter adapter = new PostAdapter(getContext(), R.layout.porst_row, postModels);
-//                    postLv.setAdapter(adapter);
-//
-//                    Object[] objectArray = new Object[2];
-//                    objectArray[0]= postModels;
-//                    objectArray[1]= db;
-//                    new WriteDatabaseTask(){
-//                    }.execute(objectArray);
-//                }
-//            }.execute();
-//            aca hacer una instancia de backend y llamar a PostIteratorListener
+        if(Backend.getInstance().isConnected(getContext()) ||
+                !Backend.getInstance().isEmpty(getContext())){
+            //si hay conexion => va a persistir
+            //si no hay conexion pero hay datos en la bd => leer de alli
             Backend.getInstance().getNextPosts(this,getContext());
         }
-
-//        listerner
-
         return v;
     }
 
     @Override
     public void nextPosts(List<PostModel> posts) {
-
-    }
-
-    public boolean isConnected(Context context) {
-        ConnectivityManager cm =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-
-        if (netInfo != null) { // tengo conexion a internet
-            return netInfo.getType() == ConnectivityManager.TYPE_WIFI ||
-                    netInfo.getType() == ConnectivityManager.TYPE_MOBILE;
-        } else {
-            return false;
-        }
+        ListView postLv = (ListView) v.findViewById(R.id.postLV);
+        PostAdapter adapter = new PostAdapter(getContext(), R.layout.porst_row, posts);
+        postLv.setAdapter(adapter);
+        //luego voy a tener q llamar a notifyDataSetChanged() en el Postadapter p/actualizar
     }
 
     public void showToast(Context c){
         CharSequence text = "No Internet connection";
-        int duration = Toast.LENGTH_SHORT;
+        int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(c, text, duration);
         toast.show();
     }
 }
-
